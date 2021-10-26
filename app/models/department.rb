19a0bc1a02c61f2b9cabdfbdd552fe7685982935
Devorah Ortega <devorahb.ortega@gmail.com>
2021-10-26 16:26:49 -0400

    Adds department validations

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,3 +1,6 @@
 class Department < ApplicationRecord
   has_many :employees
+
+  validates :dept_name, :dept_code, :dept_manager, presence: true
+  validates :dept_code, numericality: { only_integer: true, greater_than: 0 }
 end
